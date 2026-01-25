c6197aa083c9e72d246824feb2c7c57f1d8829b5
aslam1105 <aslam3117@gmail.com>
2026-01-25 17:19:56 +0530

    small changes for pull reques

--- src/product-catalog/main.go
+++ src/product-catalog/main.go
@@ -314,7 +314,8 @@ func (p *productCatalog) checkProductFailure(ctx context.Context, id string) boo
 	return failureEnabled
 }
 
-func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) {
+func createClient(ctx context.Context, svcAddr string) (*grpc.ClientConn, error) 
+{
 	return grpc.DialContext(ctx, svcAddr,
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
